c0f52657e41491720299c7e9a91eb8cc285085ce
Ziad-Abaza <zeyad.h.abaza@gmail.com>
2024-06-23 00:07:19 +0300

    first push

--- chats.php
+++ chats.php
@@ -29,7 +29,7 @@ $sql = mysqli_query($conn, "SELECT * FROM users WHERE users.id = $user_id");
       <section class="d-flex" style="height: calc(100vh - 125px);">
         <div class="w-35 d-flex " style=" margin-right:15px; ">
           <div class="w-full" style="    height: calc(100vh - 125px); display:flex; flex-direction: column; justify-content:space-between; padding:20px 0 20px 0;">
-            <div class="users-list w-full" style="height: calc(100vh - 125px); padding:0 0 0 10px;" id="sendData">
+            <div class="users-list w-full" style="height: calc(100vh - 125px); padding:0 0 0 10px;overflow: auto;" id="sendData">
             </div>
           </div>
           <div class="vertical-line"></div>
